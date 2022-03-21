8b0ef764ed21d1e67eee1b16de3912ff8ffc0120
Yarden Ankonina <yardi1994@gmail.com>
2022-03-22 01:44:17 +0200

    Add project files.

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
