218827db25cdd946964e3aa0b9eafb1396f22cef
Yarden Ankonina <yardi1994@gmail.com>
2022-03-22 06:00:40 +0200

    TwoSum

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LeetCodeSolutions.Array;
 
 namespace LeetCodeSolutions
 {
@@ -6,7 +7,8 @@ namespace LeetCodeSolutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TwoSum1 twoSum1 = new TwoSum1();
+            twoSum1.Run();
         }
     }
 }
