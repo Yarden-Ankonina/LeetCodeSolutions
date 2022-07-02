be11b1c2a19e8a68836c4aac914d8ef36a4cd189
Yarden Ankonina <yardi1994@gmail.com>
2022-07-03 02:50:04 +0300

    Anagram

---
+++ LeetCodeSolutions/Arrays/242_ValidAnagram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class _242_ValidAnagram
+    {
+        private static bool IsAnagram(string s, string t)
+        {
+            bool isAnagram = true;
+            if(s.Length == t.Length)
+            {
+                char[] s_Array = s.ToArray();
+                char[] t_Array = t.ToArray();
+
+                Array.Sort(s_Array);
+                Array.Sort(t_Array);
+
+                for(int i = 0; i < s.Length; i++)
+                {
+                    if (s_Array[i] != t_Array[i])
+                    {
+                        isAnagram = false;
+                    }
+                }
+            }
+            else
+            {
+                isAnagram = false;
+            }
+
+            return isAnagram;
+        }
+
+        public static void Test()
+        {
+            string s = "anagram", t = "nagaram";
+            Console.WriteLine(s + ", " + t);
+            Console.WriteLine(IsAnagram(s, t));
+        }
+    }
+}
--- LeetCodeSolutions/MenuCreator.cs
+++ LeetCodeSolutions/MenuCreator.cs
@@ -16,29 +16,30 @@ namespace LeetCodeSolutions
 
             MenuItem problems = new MenuItem("Problems");
 
-            MenuItem arrays = new MenuItem("Arrays");
-            MenuItem twoSumBrute = new MenuItem("1_TwoSum", _1_TwoSum.TestBrute);
+            MenuItem arraysAndHashing = new MenuItem("Arrays & Hashing");
 
-            MenuItem runningSum = new MenuItem("1480_RunningSum", _1480_RunningSum.Test);
-            MenuItem containDuplicate = new MenuItem("217_ContainsDuplicates - Brute", _217_ContainsDuplicates.TestBruteForce);
+            MenuItem twoSum = new MenuItem("1_TwoSum");
+            MenuItem twoSumBrute = new MenuItem("1_TwoSum - Brute", _1_TwoSum.TestBrute);
+            MenuItem twoSumHash = new MenuItem("1_TwoSum - Hash", _1_TwoSum.TestHash);
+            twoSum.AddSubMenu(twoSumBrute, twoSumHash);
+
+            MenuItem containDuplicates = new MenuItem("217_ContainsDuplicates");
+            MenuItem containDuplicateBrute = new MenuItem("217_ContainsDuplicates - Brute", _217_ContainsDuplicates.TestBruteForce);
             MenuItem containDuplicateSorting = new MenuItem("217_ContainsDuplicates - Sort", _217_ContainsDuplicates.TestSort);
-
-
-            MenuItem hash = new MenuItem("Hash");
-            MenuItem twoSumHash = new MenuItem("1_TwoSum", _1_TwoSum.TestHash);
             MenuItem containDuplicateHash = new MenuItem("217_ContainsDuplicates - Hashset", _217_ContainsDuplicates.TestHash);
+            containDuplicates.AddSubMenu(containDuplicateBrute, containDuplicateSorting, containDuplicateHash);
 
+            MenuItem validAnagram = new MenuItem("242_ValidAnagram", _242_ValidAnagram.Test);
+            MenuItem runningSum = new MenuItem("1480_RunningSum", _1480_RunningSum.Test);
+
+            arraysAndHashing.AddSubMenu(twoSum, containDuplicates,
+                validAnagram, runningSum);
 
             //MenuItem settings = new MenuItem("Settings");
             //MenuItem changeDefaultIntArray = new MenuItem("Change Default Int Array?", MenuSettings.changeDefaultIntArray);
             //settings.AddSubMenu(changeDefaultIntArray);
 
-            arrays.AddSubMenu(twoSumBrute, runningSum,
-                containDuplicate, containDuplicateSorting);
-
-            hash.AddSubMenu(twoSumHash, containDuplicateHash);
-
-            problems.AddSubMenu(arrays, hash);
+            problems.AddSubMenu(arraysAndHashing);
             menu.AddSubMenu(problems/*setting*/);
             menu.Show();
         }
